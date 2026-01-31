c61ddae67024aefc8378171a09d027e6bd250cd6
abnrk <adudowas@gmail.com>
2026-01-31 16:41:28 +0000

    Add files via upload

--- search.php
+++ search.php
@@ -1,56 +1,46 @@
-<?php
-require "searchconfig.php";
-
-function search(string $q,string $keyword="web",int $page=1) {
-	$opts = array('http'=>array('header' => "User-Agent: search.php/1.0\r\n"));
-	$context = stream_context_create($opts);
-	$response = json_decode(file_get_contents("https://api.qwant.com/v3/search/".$keyword."?q=".urlencode($q)."&count=10&offset=".strval(($page-1)*10)."&locale=en_us",false,$context),true);
-	$results = [];
-	if($keyword == "web") {
-		foreach($response["data"]["result"]["items"]["mainline"] as $row) {
-			if(gettype($row) == "array" && $row["type"] == "web") {
-				foreach($row["items"] as $i) {
-					array_push($results,$i);
-				}
-			}
-		}
-	} else {
-		$results = $response["data"]["result"]["items"];
-	}
-	return $results;
-}
-if(array_key_exists("q",$_GET)) {
-	$query = $_GET["q"];
-} else {
-	require "searchhome.php";
-}
-if(array_key_exists("k",$_GET)) {
-	$keyword = $_GET["k"];
-} else {
-	$keyword = "web";
-}
-if(array_key_exists("p",$_GET)) {
-	$page = intval($_GET["p"]);
-} else {
-	$page = 1;
-}
-$results = search($query,$keyword,$page);
-?>
-<p id="resultsnum"><?php echo count($results); ?> results</p>
-<div id="results">
-<?php
-foreach($results as $i) {
-	echo "<div class=\"result\">";
-	echo "<a href=\"".$i["url"]."\">".htmlspecialchars($i["title"])."</a>";
-	if(isset($i["desc"])) {
-		echo "<p>".htmlspecialchars($i["desc"])."</p>";
-	}
-	if(isset($i["media"])) {
-		echo "<br><img src=\"".$i["media"]."\" alt=\"".htmlspecialchars($i["title"])."\" width=\"200\">";
-	}
-	echo "</div>";
-}
-?>
-</div>
-<hr>
-<nav id="footer"><a href="?">Search</a> | <a href="<?php echo "?q=".$_GET["q"]."&k=".$_GET["k"]."&p=".strval($page-1); ?>">Previous</a> | <a href="<?php echo "?q=".$_GET["q"]."&k=".$_GET["k"]."&p=".strval($page+1); ?>">Next</a> | Getting bad results? Try refreshing the page.</nav>
+<html>
+  <head>
+    <?php
+    $search = urlencode(htmlspecialchars($_GET["q"]));
+    if(isset($_GET['tbm'])){
+      if (htmlspecialchars($_GET["tbm"]) == 'isch') {
+        $mode = 'img';
+      }
+      }else{
+        $mode = 'all';
+      }
+    ?>
+    <!-- <title><?php echo $search . ' - PHP Proxy'?></title> -->
+  </head>
+  <body>
+    <?php
+    $opts = [
+     "http" => [
+       "header" => "User-Agent: Opera/9.80 (J2ME/MIDP; Opera Mini/8.0; U; en) Presto/2.12.423 Version/12.16",
+     ]
+    ];
+    $context = stream_context_create($opts);
+    if ($mode == 'all') {
+      if (isset($_GET['start'])) {
+          $data = file_get_contents('https://www.google.com/search?q=' . $search . "&start=" . $_GET['start'],false,$context);
+      } else {
+          $data = file_get_contents('https://www.google.com/search?q=' . $search,false,$context);
+      }
+      $data = str_replace("href=\"/search", "href=\"/search.php", $data);
+      $data = str_replace("href=\"/?sa", "href=\"/google.php?sa", $data);
+      $data = str_replace("href=\"/url", "href=\"/url.php", $data);
+    } elseif ($mode == 'img') {
+      if (isset($_GET['start'])) {
+          $data = file_get_contents('https://www.google.com/search?q=' . $search . '&tbm=isch' . "&start=" . $_GET['start'],false,$context);
+      } else {
+          $data = file_get_contents('https://www.google.com/search?q=' . $search . '&tbm=isch',false,$context);
+      }
+      $data = str_replace("href=\"/search", "href=\"/search.php", $data);
+      $data = str_replace("href=\"/?sa", "href=\"/google.php?sa", $data);
+      $data = str_replace("href=\"/url", "href=\"/url.php", $data);
+    }
+    $data = preg_replace('/<div data-ved="[0-9a-zA-Z]*">.*AI Overview[ -~]*<\/span><\/div><\/div><\/div>/',"",$data);
+    echo $data;
+    ?>
+  </body>
+</html>
--- url.php
+++ url.php
@@ -1,11 +1,5 @@
-<html>
-  <head>
-  <?php
-  $url = htmlspecialchars($_GET["q"]);
-  header('Location: ' . $url);
-  exit;
-  ?>
-  </head>
-  <body>
-  </body>
-</html>
+<?php
+$url = htmlspecialchars($_GET["q"]);
+header('Location: ' . $url);
+exit;
+?>
